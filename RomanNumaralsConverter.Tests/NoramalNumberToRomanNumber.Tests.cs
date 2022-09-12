6b39eed9b3dcb59b075d8dabdd67693d59aa7962
Jebumon <jebumon@gmail.com>
2022-09-12 01:42:00 +0100

    Completed: Part1, Added more test cases all test cases pased!

--- RomanNumaralsConverter.Tests/NoramalNumberToRomanNumber.Tests.cs
+++ RomanNumaralsConverter.Tests/NoramalNumberToRomanNumber.Tests.cs
@@ -37,6 +37,42 @@ public class NoramalNumberToRomanNumberTests
     }
 
     [Test]
+    public void Convert_number_2221_should_return_MMCCXXI()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(2221).Should().Be("MMCCXXI");
+    }
+
+    [Test]
+    public void Convert_number_1249_should_return_MCCXLIX()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(1249).Should().Be("MCCXLIX");
+    }
+
+    [Test]
+    public void Convert_number_2290_should_return_MMCCXC()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(2290).Should().Be("MMCCXC");
+    }
+
+    [Test]
+    public void Convert_number_1234_should_return_MCCXXXIV()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(1234).Should().Be("MCCXXXIV");
+    }
+
+    [Test]
+    public void Convert_number_2345_should_return_MMCCCXLV()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(2345).Should().Be("MMCCCXLV");
+    }
+
+    [Test]
+    public void Convert_number_94_should_return_XCIV()
+    {
+        NoramalNumberToRomanNumber.ConverNumber(94).Should().Be("XCIV");
+    }
+
+    [Test]
     public void Convert_number_0_should_return_ArgumentException()
     {
         var ex = Assert.Throws<ArgumentException>(() => NoramalNumberToRomanNumber.ConverNumber(0));
