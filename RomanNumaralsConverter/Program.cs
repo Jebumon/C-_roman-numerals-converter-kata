56279e4fd41b7d2cc5b1543853579885da9c1428
Jebumon <jebumon@gmail.com>
2022-09-13 01:20:10 +0100

    Completed : Some changes in console appearence.

--- RomanNumaralsConverter/Program.cs
+++ RomanNumaralsConverter/Program.cs
@@ -1,10 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RomanNumaralsConverter.Models;
-Console.WriteLine("Hello, World!");
 NoramalNumberToRomanNumber noramalNumberToRoman = new();
-Console.WriteLine("Please enter a number");
+RomanNumberToNormalNumber romanNumberToNormalNumber = new(null);
+Console.WriteLine("---------------------------------------------------------------------------");
+Console.WriteLine("|                         Roman numeral converter                         |");
+Console.WriteLine("---------------------------------------------------------------------------");
+Console.WriteLine("|                                                                         |\n");
+Console.WriteLine("|           Press '1' for Convert Normal number to Roman number           |");
+Console.WriteLine("|                                                                         |\n");
+Console.WriteLine("|           Press '2' for Convert Roman number to Normal number           |");
+Console.WriteLine("|                                                                         |\n");
+Console.WriteLine("|                      Please select an option :                          |");
 var  a = Console.ReadLine();
 int number = Convert.ToInt32(a);
-string romanNumber = noramalNumberToRoman.ConverNumber(number);
+if (number == 1) 
+{
+    Console.WriteLine("|                                                                        |\n");
+    Console.WriteLine("|                   Please ente a Normal number                          |");
+    Console.WriteLine("|                                                                        |\n");
+    var userInputNumber = Console.ReadLine();
+    int inputNumberInt = Convert.ToInt32(userInputNumber);
+    string romanNumber = noramalNumberToRoman.ConverNumber(inputNumberInt);
+    Console.WriteLine("|        You Converted " + "'" + inputNumberInt + "'" + " to Roman Number " + "'" + romanNumber + "'          |");
+    Console.WriteLine("|                                                                         |\n");
+    Console.WriteLine("---------------------------------------------------------------------------");
+}
+else if(number == 2) 
+{
+    Console.WriteLine("|                                                                        |\n");
+    Console.WriteLine("|                   Please ente a roman number                           |");
+    Console.WriteLine("|                                                                        |\n");
+    var userInputNumber = Console.ReadLine();
+    string inputNumberString = Convert.ToString(userInputNumber).ToUpper();
+    int normalNumber = romanNumberToNormalNumber.ConvertRomanNumber(inputNumberString);
+    //Console.WriteLine("                You Converted " + "'" + inputNumberString + "'" + " to normal Number " + "'" + normalNumber + "'");
+    Console.WriteLine("|                                                                         |\n");
+    Console.WriteLine("---------------------------------------------------------------------------");
+}
 
-Console.WriteLine("You Converted " +"'"+number+"'" + " to Roman Number "+ "'"+ romanNumber+ "'");
+
+
